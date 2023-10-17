c486d351c80feca16afbd78adffc793f2b514f46
AniolENTI <aniol.morato@enti.cat>
2023-10-17 09:41:46 +0200

    Popups 1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.myapplication
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.SharedPreferences
+import android.hardware.biometrics.BiometricManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -19,13 +25,55 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.core.widget.doBeforeTextChanged
+//import com.example.myapplication.Utils.SharedPreferencesManager
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
 
+    //val openTimesAppText by lazy { findViewById<TextView>(R.id.open_app_times_text) }
+    val testBt by lazy { findViewById<Button>(R.id.test_bt) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_screen)
 
+        //SharedPreferencesManager.TimesOpenApp++
+        //openTimesAppText.text = SharedPreferencesManager.TimesOpenApp.toString()
+
+        testBt.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Sóc un Alert molt guai")
+            builder.setMessage("Sóc el missatge d'un Alert molt guai")
+
+            builder.setPositiveButton("OK") { dialog, which, -> dialog.dismiss()}
+            builder.setNegativeButton("NO") { dialog, which, -> dialog.dismiss()}
+            builder.setNeutralButton("NEUTRAL") { dialog, which, -> dialog.dismiss()}
+
+            val options = arrayOf("Resposta 1", "Resposta 2", "Resposta 3", "Resposta 4")
+            val selectedItems = booleanArrayOf(false, false, false ,false)
+
+            //builder.setMultiChoiceItems(options, selectedItems) {dialog, which, isChecked -> selectedItems[which] = isChecked}
+
+            builder.setOnCancelListener{
+                val toast = Toast(this)
+                toast.setText("Sóc un toast molt guai")
+                toast.show()
+            }
+
+            val linear = LinearLayout(this)
+            linear.orientation = LinearLayout.VERTICAL
+
+            val input = EditText(this)
+            input.hint = "Escriu aquí"
+
+            val input2 = EditText(this)
+            input2.hint = "Escriu aquí"
+
+            linear.addView(input)
+            linear.addView(input2)
+
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Utils/SharedPreferencesManager.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.Utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object SharedPreferencesManager {
+    private const val MyPreferencesName = "MySharedPreferencesStorage"
+    private const val TimesOpenAppKey = "AppOpenTimes"
+    private val shared: SharedPreferences by lazy { MyApp.Context.getSharedPreferences(
+        MyPreferencesName, Context.MODE_PRIVATE) }
+    private val editor: SharedPreferences.Editor by lazy { shared.edit() }
+
+
+    public var TimesOpenApp: Int
+        get() = shared.getInt(TimesOpenAppKey, 0)
+        set(value) {
+            editor.putInt(TimesOpenAppKey, value)
+            editor.apply()
+        }
+}
